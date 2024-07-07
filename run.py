6595d8a3c7a6a45c2a31add7d6b878d2773730f5
sita8281 <mrsit8281@yandex.ru>
2024-07-08 02:48:46 +0300

    commit run.py

--- run.py
+++ run.py
@@ -2,6 +2,9 @@ from app import app, db
 from app.models import User, Host, HostFolder
 
 
+# runner
+
+
 @app.shell_context_processor
 def make_shell_context():
     return {'app': app, 'db': db, 'User': User, 'Host': Host, 'HostFolder': HostFolder}
