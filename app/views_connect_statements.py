c8fc64ec02769368e1a19e9a67c89c2d45c010a6
artem8281 <mrsit8281@yandex.ru>
2024-07-23 04:30:04 +0300

    big FIXING and Develop CfgStorage

--- app/views_connect_statements.py
+++ app/views_connect_statements.py
@@ -4,11 +4,16 @@ from flask_login import current_user, login_required
 from sqlalchemy.exc import SQLAlchemyError
 from datetime import datetime
 import pickle
+import logging
+
+
+log = logging.getLogger('connect_statements')
 
 
 @app.route('/connect_statements', methods=['GET'])
 @login_required
 def connect_statements():
+    log.info(f'{current_user.login}: посетил страницу')
     return render_template('/connect_statements/index.html')
 
 
@@ -20,11 +25,13 @@ def connect_statements_list(endp):
         statements = db.session.query(
             models.ConnectStatements
         ).filter(models.ConnectStatements.status >= 1).all()
+        log.info(f'{current_user.login}: запросил список открытых и ожидающих ответа заявок')
     elif endp == 'close':
         # получить закрытые заявки
         statements = db.session.query(
             models.ConnectStatements
         ).filter(models.ConnectStatements.status == 0).all()
+        log.info(f'{current_user.login}: запросил список закрытых заявок')
     else:
         return abort(404)
     return jsonify([statement.to_serializeble for statement in statements][::-1])
@@ -56,7 +63,9 @@ def connect_statements_add():
         db.session.commit()
     except SQLAlchemyError:
         db.session.rollback()
+        log.info(f'{current_user.login}: ошибка БД при создании заявки')
         return abort(500)
+    log.info(f'{current_user.login}: создал новую заявку <{form.get("name")}>')
     return 'OK', 200
 
 
@@ -67,6 +76,7 @@ def connect_statements_resource(iid):
     statement = db.session.query(models.ConnectStatements).get_or_404(iid)
     if request.method == 'GET':
         messages: list = pickle.loads(statement.messages)
+        log.info(f'{current_user.login}: запросил данные заявки <{statement.name}> | <id={statement.id}>')
         return jsonify({
             'messages': messages,
             'name': statement.name,
@@ -80,6 +90,8 @@ def connect_statements_resource(iid):
         messages: list = pickle.loads(statement.messages)
         messages.append({'sender': current_user.login, 'date': date, 'message': form['message']})
         statement.messages = pickle.dumps(messages)
+        log.info(f'{current_user.login}: отправил сообщение <{form["message"]}>'
+                 f' в заявку <{statement.name}> | <id={statement.id}>')
     if form.get('status'):
         statement.status = int(form['status'])
     if form.get('for_whom'):
