1b486ff0aaf38b53e2471784ac7ee4ca096b140c
sita8281 <mrsit8281@yandex.ru>
2024-07-28 19:30:57 +0300

    developing web_session_maker

--- app/modules/web_session_maker.py
+++ app/modules/web_session_maker.py
@@ -1,5 +1,6 @@
 from threading import Thread
 import time
+import uuid
 
 
 class SessionMaker(Thread):
@@ -9,24 +10,25 @@ class SessionMaker(Thread):
     def run(self) -> None:
         print(' * Web Sessions Daemon: on')
         while True:
-            time.sleep(30)
+            time.sleep(1)
             try:
                 for key, val in self._online_users.items():
-                    if time.time() - val[1] > 5:
-                        self.disconnect(key)
+                    if time.time() - val[1] > 10:
+                        self._disconnect(key)
             except Exception:
                 pass
 
     def connect(self, session_token: str, remote_ip: str) -> None:
-        self._online_users[session_token] = [remote_ip, int(time.time()), 'connect']
+        self._online_users[session_token] = [remote_ip, int(time.time()), 'connect', str(uuid.uuid4())]
 
-    def disconnect(self, session_token: str) -> None:
+    def _disconnect(self, session_token: str) -> None:
         if session_token in self._online_users:
             del self._online_users[session_token]
 
-    def disconnect_signal(self, session_token) -> None:
+    def logout_signal(self, session_token) -> None:
         if session_token in self._online_users:
             self._online_users[session_token][2] = 'logout'
+
     @property
     def get_connections(self) -> dict:
         return self._online_users
--- app/views_online_users.py
+++ app/views_online_users.py
@@ -4,20 +4,28 @@ from flask_login import login_required, login_user, logout_user, current_user
 from .modules import sessions_online
 
 
-@app.route('/is_online', methods=['POST'])
+@app.route('/connected', methods=['POST'])
 @login_required
 def is_online():
+    host = request.headers.get('X-Real-Ip')
+    if not host:
+        host = request.remote_addr
+
     sessions_online.connect(
         session_token=request.cookies.get('session'),
-        #remote_ip=str(request.headers.get('X-Real-Ip'))
-        remote_ip=request.remote_addr
+        remote_ip=host,
     )
     return 'Connected', 200
 
 
-@app.route('/online', methods=['GET'])
+@app.route('/online_users', methods=['GET'])
 @login_required
 def users_online():
-    print(sessions_online.get_connections)
     return jsonify(sessions_online.get_connections), 200
+
+
+@app.route('/disconnect', methods=['POST'])
+@login_required
+def user_disconnect():
+    return jsonify('notify', 'Вы отключили пользователя')
 
