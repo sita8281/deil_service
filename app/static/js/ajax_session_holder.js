8e4bc2c4e36eda2e32bdbb46af169124a64ce8bf
artem8281 <mrsit8281@yandex.ru>
2024-07-29 00:06:22 +0300

    add JS file -> session_holder

---
+++ app/static/js/ajax_session_holder.js
@@ -0,0 +1,26 @@
+
+/*
+Для удобства этот JS подключается к Header-Menu, так это меню присутсвует на всех страница (почти)
+Так проще трекать онлайн сейчас User или нет
+*/
+
+function sessionHold() {
+    $.ajax({
+        type: "post",
+        url: "/connected",
+        dataType: "text",
+        timeout: 3000,
+        success: function (response) {
+            console.log(response);
+            if (response == 'logout') {
+                window.location = '/logout';
+            }
+        }
+    });
+}
+
+sessionHold();
+
+setInterval(() => {
+    sessionHold();
+}, 5000);
