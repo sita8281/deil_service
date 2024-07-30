052d3b11dff4755dd2426fd57f05e2d2c989217a
sita8281 <mrsit8281@yandex.ru>
2024-07-30 17:14:35 +0300

    finally Replace Statements Algoritm

--- app/views_connect_statements.py
+++ app/views_connect_statements.py
@@ -108,7 +108,7 @@ def connect_statements_resource(iid):
 @app.route('/connect_statements/replace', methods=['POST'])
 @login_required
 def statements_replace():
-    stmts = db.session.query(models.ConnectStatements).all()
+    stmts = db.session.query(models.ConnectStatements).filter(models.ConnectStatements.status >= 1).all()
     drag = request.form.get('drag')
     drop = request.form.get('drop')
     if 'id' in drag and 'sep' in drop:
@@ -116,13 +116,24 @@ def statements_replace():
         drop_id = int(drop.split('-')[1].strip())
 
         if drag_id > drop_id:
-            print(drag_id, drop_id)
-    for i in stmts:
-        print(i.lst)
-        if i.id == 31:
-            i.lst = ['1', '2', '3', '4', b'0']
-    # db.session.flush()
-    # db.session.commit()
+            for i in reversed(range(len(stmts))):
+                if stmts[i].id < drag_id:
+                    stmts[i+1].lst, stmts[i].lst = stmts[i].lst, stmts[i+1].lst
+                if stmts[i].id == drop_id:
+                    break
+
+        if drag_id < drop_id:
+            for i in range(len(stmts)):
+                if stmts[i].id > drag_id:
+                    stmts[i-1].lst, stmts[i].lst = stmts[i].lst, stmts[i-1].lst
+                if stmts[i].id == drop_id:
+                    break
+
+
+
+
+    db.session.flush()
+    db.session.commit()
 
     return 'OK', 200
 
